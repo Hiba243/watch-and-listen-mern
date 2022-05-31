1f0c0d36590d0a5b9e961baa288d4c6cb5fbeaaa
Hiba243 <hibafatima24@gmail.com>
2022-05-31 16:29:29 +0530

    added deployment details

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path =require('path');
 connectDB();
 const app = express();
 
@@ -12,6 +13,14 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the Watch and Listen API'
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/video', require('./routes/video'));
+
+//serve react in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
