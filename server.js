85de27c42d51cb547111daa52fc0554da8390143
Hiba243 <hibafatima24@gmail.com>
2022-06-01 12:35:58 +0530

    updated server.js

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || '5000';
 const host = '0.0.0.0';
 app.listen(PORT, host, () => console.log(`Server started on port ${PORT}`));
