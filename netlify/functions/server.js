ea009d4df6ac585f08799981ff98a7ad4715e38c
Hiba Fatima <hibafatima24@gmail.com>
2023-08-04 18:54:50 +0530

    fix to deploy to netlify

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const connectDB = require('../../connDb/db');
 const path = require('path');
 const serverless = require("serverless-http");
-
 const app = express();
+
+const db = "mongodb+srv://123hiba:123hiba@watchandlistencluster.ldcaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
 
 // Connect Database
 connectDB();
