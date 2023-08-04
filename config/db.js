84d9f6b963dc45a686cacc37880dd4b2be980a85
Hiba Fatima <hibafatima24@gmail.com>
2023-08-04 16:13:35 +0530

    fix to deploy to netlify

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = "mongodb+srv://123hiba:123hiba@watchandlistencluster.ldcaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
